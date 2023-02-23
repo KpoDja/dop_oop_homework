8353f649b7fe1efd0936d7d29394c4a8a62fad94
Yury Larin <Djamtter@gmail.com>
2023-02-23 22:11:43 +0300

    change

--- main.py
+++ main.py
@@ -12,22 +12,6 @@ class Figures:
             self.color = color
             return f"Теперь фигрура цвета {self.color}"
 
-    def perimeter_triangle(self):
-        pt = self.side_a + self.side_b + self.side_c
-        return f"Периметр этого треугольника равна {pt}"
-
-    def square_triangle(self):
-        st = (self.side_a + self.side_b + self.side_c) / 2
-        return f"Площадь этого треугольника равна {st}"
-
-    def perimeter_rectangle(self):
-        pr = 2 * (self.side_a + self.side_b)
-        return f"Периметр этого прямоугольника равен {pr}"
-
-    def square_rectangle(self):
-        sr = self.side_a * self.side_b
-        return f"Площадь этого прямоугольника равна {sr}"
-
 
 class Triangle(Figures):
     def __init__(self, color, side_a, side_b, side_c):
@@ -35,6 +19,14 @@ class Triangle(Figures):
 
     def info_triangle(self):
         return f"Это треугольник со сторонами равными: {self.side_a}, {self.side_b}, {self.side_c} и цветом {self.color}"
+
+    def perimeter(self):
+        pt = self.side_a + self.side_b + self.side_c
+        return f"Периметр этого треугольника равна {pt}"
+
+    def square(self):
+        st = (self.side_a + self.side_b + self.side_c) / 2
+        return f"Площадь этого треугольника равна {st}"
 
 
 class Rectangle(Figures):
@@ -44,18 +36,26 @@ class Rectangle(Figures):
     def info_rectangle(self):
         return f"Это прямоугольник со сторонами: {self.side_a}, {self.side_b}, {self.side_a}, {self.side_b} и цветом {self.color}"
 
+    def perimeter(self):
+        pr = 2 * (self.side_a + self.side_b)
+        return f"Периметр этого прямоугольника равен {pr}"
+
+    def square(self):
+        sr = self.side_a * self.side_b
+        return f"Площадь этого прямоугольника равна {sr}"
+
 
 if __name__ == "__main__":
     fig_t = Triangle("red", 2.1, 2, 4.3)
-    print(fig_t.perimeter_triangle())
-    print(fig_t.square_triangle())
+    print(fig_t.perimeter())
+    print(fig_t.square())
     print(fig_t.painting("red"))
     print(fig_t.painting("blue"))
     print(fig_t.info_triangle())
 
     fig_r = Rectangle("yellow", 2.2, 4)
-    print(fig_r.perimeter_rectangle())
-    print(fig_r.square_rectangle())
+    print(fig_r.perimeter())
+    print(fig_r.square())
     print(fig_r.painting("yellow"))
     print(fig_r.painting("green"))
     print(fig_r.painting("yellow"))
